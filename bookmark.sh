bcb9af23d51d949d81a5f23f4b19566ac80af51e
Jeff Parent <jeff@commentedcode.org>
2015-10-09 13:28:33 -0500

    Bumped version to 0.4

--- bookmark.sh
+++ bookmark.sh
@@ -39,7 +39,7 @@
 #        AUTHOR: Jeff Parent (jeff@commentedcode.org
 #  ORGANIZATION: 
 #       CREATED: 09/25/2015 11:39
-#      REVISION: 0.3
+#      REVISION: 0.4
 #
 # ACKNOWLEDGEMENTS:
 # bash_cgi
@@ -86,7 +86,7 @@ LINK_DB="${DB_DIR}/links.db"
 touch "${LINK_DB}"
 
 # Version, releases are X.Y, dev are X.Y.Z
-VERSION=0.3
+VERSION=0.4
 
 ##################
 # START bash_cgi #
